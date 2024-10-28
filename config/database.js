efc915faaf80d54bd3ad7e815d0179b4ccc6d959
nhanralph <dnhnhan2003@gmail.com>
2024-10-29 01:39:55 +0700

    config db server

--- config/database.js
+++ config/database.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
-//const mysql = require('mysql2');
+
+//Database Local
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.MYSQL_DB,
+//   // waitForConnections: true,
+//   // connectionLimit: 10,
+//   // queueLimit: 0
+// });
+
+//Database server
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0
-});
+  password:process.env.DB_PASS,
+  database:process.env.MYSQL_DB,
+  port:process.env.DB_PORT,
+})
+; 
 
 module.exports = pool;
+
