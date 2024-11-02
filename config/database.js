a0a69ebf77d7bc09b2d40893c44775309b703eb3
nhanralph <dnhnhan2003@gmail.com>
2024-11-02 22:11:43 +0700

    change db by server

--- config/database.js
+++ config/database.js
@@ -2,25 +2,25 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 //Database Local
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0
-});
-
-//Database server
 // const pool = mysql.createPool({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
-//   password:process.env.DB_PASS,
-//   database:process.env.MYSQL_DB,
-//   port:process.env.DB_PORT,
-// })
-// ; 
+//   password: process.env.DB_PASS,
+//   database: process.env.MYSQL_DB,
+//   // waitForConnections: true,
+//   // connectionLimit: 10,
+//   // queueLimit: 0
+// });
+
+//Database server
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password:process.env.DB_PASS,
+  database:process.env.MYSQL_DB,
+  port:process.env.DB_PORT,
+})
+; 
 
 module.exports = pool;
 
